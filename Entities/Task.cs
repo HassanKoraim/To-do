0b949fc8823bcbbc7cd8f96da364d88ccf275cb4
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-24 21:38:06 +0200

    Add Some Editions in Task class

--- Entities/Task.cs
+++ Entities/Task.cs
@@ -1,14 +1,20 @@
-﻿namespace Entities
+﻿using Entities.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities
 {
     public class Task
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime CreatedTime { get; set; } = DateTime.Now;
         public DateTime UpdatedTime { get; set;} = DateTime.Now;
+        [Required]
         public DateTime DueTime { get; set; }
-        public int Status { get; set; }
+        public StatusOptions Status { get; set; } = StatusOptions.NotCompleted;
 
     }
 }
