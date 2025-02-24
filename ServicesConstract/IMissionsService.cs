3b39cae24c60220bc47d0c7feb62c1f0df9f45e0
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-24 23:59:25 +0200

    Create 'IMissionService' Interface

---
+++ ServicesConstract/IMissionsService.cs
@@ -0,0 +1,14 @@
+﻿
+using Entities;
+using ServicesConstract.DTO;
+
+namespace ServicesConstract
+{
+    public interface IMissionsService
+    {
+        List<MissionResponse> GetAllMissions();
+        MissionResponse AddMission(MissionAddRequest missionAddRequest);
+        MissionResponse UpdateMission();
+        bool DeleteMission(int Id);
+    }
+}
