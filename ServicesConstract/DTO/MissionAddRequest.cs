b0728a49f35aa4ee69f872ceff2e97601c0e74f4
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-24 23:45:55 +0200

    Create 'MissionAddRequest' DTO Class

---
+++ ServicesConstract/DTO/MissionAddRequest.cs
@@ -0,0 +1,30 @@
+﻿using Entities;
+using Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesConstract.DTO
+{
+    public class MissionAddRequest
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+        [Required]
+        public DateTime DueTime { get; set; }
+        public StatusOptions Status { get; set; } = StatusOptions.NotCompleted;
+
+        public Mission ToMission()
+        {
+            return new Mission()
+            { Id = Id, Title = Title, Description = Description, CreatedTime = CreatedTime, DueTime = DueTime, Status = Status };
+        }
+    }
+}
