2b5b5832c062970aa7df4e406b97192fbdae4654
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-24 21:34:14 +0200

    Add Entities and Task class

---
+++ Entities/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedTime { get; set; } = DateTime.Now;
+        public DateTime UpdatedTime { get; set;} = DateTime.Now;
+        public DateTime DueTime { get; set; }
+        public int Status { get; set; }
+
+    }
+}
