24e962e4710d4f85681078c2fd4843c461da1b02
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-25 13:07:03 +0200

    Inhirte from 'INotesService' Interface and Implement 'AddMission' Method

---
+++ Services/NotesService.cs
@@ -0,0 +1,36 @@
+﻿using Entities;
+using Services.Helpers;
+using ServicesConstract;
+using ServicesConstract.DTO;
+
+namespace Services
+{
+    public class NotesService : INotesService
+    {
+        private readonly NotesDbContext _db;
+        public NoteResponse AddMission(NoteAddRequest noteAddRequest)
+        {
+            if(noteAddRequest == null)
+                throw new ArgumentNullException(nameof(noteAddRequest));
+            ValidationHelper.ModelValidation(nameof(noteAddRequest));
+            Note note = noteAddRequest.ToNote();
+            _db.Add(note);
+            return note.ToNoteResponse();
+        }
+
+        public bool DeleteMission(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<NoteResponse> GetAllMissions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public NoteResponse UpdateMission()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
