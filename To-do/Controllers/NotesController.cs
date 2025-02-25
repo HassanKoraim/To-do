9c7d0f99956e8969ce3a8551f49343eaefbb43ed
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-25 19:47:06 +0200

    Create 'NotesController' Controller

---
+++ To-do/Controllers/NotesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServicesConstract;
+using ServicesConstract.DTO;
+
+namespace To_do.Controllers
+{
+    [Controller]
+    public class NotesController : Controller
+    {
+        private readonly INotesService _notesService;
+        public NotesController(INotesService notesService)
+        {
+            _notesService = notesService;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            List<NoteResponse> notesResponse = _notesService.GetAllNotes();
+            return View(notesResponse);
+        }
+    }
+}
