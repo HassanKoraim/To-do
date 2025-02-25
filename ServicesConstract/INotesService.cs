d777770d70098b6744d53087af9e3e49f362fb32
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-25 13:11:57 +0200

    Change All 'Mission' to 'Note' in all methods

---
+++ ServicesConstract/INotesService.cs
@@ -0,0 +1,14 @@
+﻿
+using Entities;
+using ServicesConstract.DTO;
+
+namespace ServicesConstract
+{
+    public interface INotesService
+    {
+        List<NoteResponse> GetAllNotes();
+        NoteResponse AddNote(NoteAddRequest noteAddRequest);
+        NoteResponse UpdateNote();
+        bool DeleteNote(int Id);
+    }
+}
