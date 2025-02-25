609a8b77b20ea8c281fbe0a09815ee6fd5541d2e
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-25 19:26:01 +0200

    Implement 'GetAllNotes' Method

--- Services/NotesService.cs
+++ Services/NotesService.cs
@@ -8,24 +8,30 @@ namespace Services
     public class NotesService : INotesService
     {
         private readonly NotesDbContext _db;
-        public NoteResponse AddNote(NoteAddRequest noteAddRequest)
+        public NotesService(NotesDbContext notesDbContext)
+        {
+            _db = notesDbContext;
+        }
+        public NoteResponse AddNote(NoteAddRequest? noteAddRequest)
         {
             if(noteAddRequest == null)
                 throw new ArgumentNullException(nameof(noteAddRequest));
             ValidationHelper.ModelValidation(nameof(noteAddRequest));
             Note note = noteAddRequest.ToNote();
-            _db.Add(note);
+            _db.Notes.Add(note);
             return note.ToNoteResponse();
         }
-
-        public bool DeleteNote(int Id)
+        
+        public bool DeleteNote(int? Id)
         {
             throw new NotImplementedException();
         }
 
         public List<NoteResponse> GetAllNotes()
         {
-            throw new NotImplementedException();
+            List<Note> notes = _db.Notes.ToList();
+            List<NoteResponse> notesResponse = notes.Select(temp => temp.ToNoteResponse()).ToList();
+            return notesResponse;
         }
 
         public NoteResponse UpdateNote()
