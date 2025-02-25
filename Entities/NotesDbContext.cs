f3a4a2529fe5e84734f2c15d81112d2e9149f839
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-26 00:46:20 +0200

    Refactor class

---
+++ Entities/NotesDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class NotesDbContext : DbContext
+    {
+        public NotesDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Note> Notes { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Note>().ToTable("Notes");
+        }
+    }
+}
