6b339ec9ed938bb4ef4439f41cc67769dbfc763a
Hassan Koraim <hassankoraim2@gmail.com>
2025-02-25 13:13:20 +0200

    Change 'ToMissionResponse' into 'ToNoteResponse' Method

---
+++ ServicesConstract/DTO/NoteResponse.cs
@@ -0,0 +1,39 @@
+﻿using Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Entities;
+
+namespace ServicesConstract.DTO
+{
+    public class NoteResponse
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedTime { get; set; }
+        [Required]
+        public DateTime DueTime { get; set; }
+        public StatusOptions Status { get; set; }
+    }
+    public static class MissionResponseExtension
+    {
+        public static NoteResponse ToNoteResponse( this Note mission)
+        {
+            return new NoteResponse()
+            {
+                Id = mission.Id,
+                Title = mission.Title,
+                Description = mission.Description,
+                CreatedTime = mission.CreatedTime,
+                DueTime = mission.DueTime,
+                Status = mission.Status
+            };
+        }
+    }
+}
