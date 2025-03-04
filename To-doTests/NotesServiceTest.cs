9b9dfe3e99b095d39a4de1da5a44306c46c9d9c6
Hassan Koraim <hassankoraim2@gmail.com>
2025-03-04 19:25:59 +0200

    Add Testing for 'GetAllNotes', 'GetNoteByNoteId', 'UpdateNote' and 'DeleteNote' Methods in the 'NotesService' class

--- To-doTests/NotesServiceTest.cs
+++ To-doTests/NotesServiceTest.cs
@@ -9,6 +9,7 @@ using FluentAssertions;
 using AutoFixture;
 using AutoFixture.Kernel;
 using System.ComponentModel.DataAnnotations;
+using ServiceConstracts.DTO;
 
 namespace To_doTests
 {
@@ -83,8 +84,231 @@ namespace To_doTests
             notesResponse_list_from_get.Should().ContainEquivalentOf(noteResponse); //Third Test
 
         }
-        #endregion 
+        #endregion
 
+        #region GetAllNotes
+
+        [Fact]
+        public void GetAllNotes_EmptyList()
+        {
+            //Arrange 
+            List<NoteResponse>? notesResponse_from_get = _notesService.GetAllNotes();
+
+            // Assert
+            notesResponse_from_get.Should().BeNullOrEmpty();
+        }
+
+        [Fact]
+        public void GetAllNotes_AddFewNotes()
+        {
+            //Arrange 
+            NoteAddRequest noteAddRequest1 = _fixture.Create<NoteAddRequest>();
+            NoteAddRequest noteAddRequest2 = _fixture.Create<NoteAddRequest>();
+            NoteAddRequest noteAddRequest3 = _fixture.Create<NoteAddRequest>();
+            NoteAddRequest noteAddRequest4 = _fixture.Create<NoteAddRequest>();
+            NoteResponse noteResponse1 = _notesService.AddNote(noteAddRequest1);
+            NoteResponse noteResponse2 = _notesService.AddNote(noteAddRequest2);
+            NoteResponse noteResponse3 = _notesService.AddNote(noteAddRequest3);
+            NoteResponse noteResponse4 = _notesService.AddNote(noteAddRequest4);
+            List<NoteResponse> notesResponse_list = 
+                new List<NoteResponse> { noteResponse1, noteResponse2, noteResponse3, noteResponse4};
+            // Act
+            List<NoteResponse>? notesResponse_list_from_get = _notesService.GetAllNotes();
+
+            //Assert
+            notesResponse_list_from_get.Should().BeEquivalentTo(notesResponse_list);
+        }
+        #endregion
+
+        #region GetNoteByNoteId
+
+        [Fact]
+        public void GetNoteByNoteId_IdIsNull()
+        {
+            // Arrange
+            Guid? Id = null;
+
+            //Act
+            Action action = () => _notesService.GetNoteByNoteId(Id);
+
+            //Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void GetNoteByNoteId_IdIsNotRight()
+        {
+            // Arrange
+            Guid? Id = Guid.NewGuid();
+
+            //Act
+            NoteResponse noteResponse = _notesService.GetNoteByNoteId(Id);
+
+            //Assert
+            noteResponse.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetNoteByNoteId_ProperDetails()
+        {
+            // Arrange
+            NoteAddRequest noteAddRequest = _fixture.Create<NoteAddRequest>();
+            NoteResponse noteResponse = _notesService.AddNote(noteAddRequest);
+
+            //Act
+            NoteResponse noteResponse_from_get = _notesService.GetNoteByNoteId(noteResponse.Id);
+
+            //Assert
+            noteResponse_from_get.Should().BeEquivalentTo(noteResponse);
+        }
+        #endregion
+
+        #region UpdateNote
+        [Fact]
+        public void UpdateNote_NullNoteUpdateRequest()
+        {
+            // Arrrange
+            NoteUpdateRequest? noteUpdateRequest = null;
+
+            //Act 
+            Action action = () => _notesService.UpdateNote(noteUpdateRequest);
+
+            // Assert
+            action.Should().Throw<ArgumentNullException>();
+
+        }
+
+        [Fact]
+        public void UpdateNote_NullTitle()
+        {
+            // Arrrange
+            NoteUpdateRequest? noteUpdateRequest = 
+                _fixture.Build<NoteUpdateRequest>()
+                .With(tem => tem.Title, null as string)
+                .Create();
+            //Act 
+            Action action = () => _notesService.UpdateNote(noteUpdateRequest);
+
+            // Assert
+            action.Should().Throw<ArgumentException>();
+
+        }
+
+        [Fact]
+        public void UpdateNote_NullDueTime()
+        {
+            // Arrrange
+            NoteUpdateRequest? noteUpdateRequest =
+                _fixture.Build<NoteUpdateRequest>()
+                .With(tem => tem.DueTime, (DateTime?)null)
+                .Create();
+            //Act 
+            Action action = () => _notesService.UpdateNote(noteUpdateRequest);
+
+            // Assert
+            action.Should().Throw<ArgumentException>();
+        }
+        [Fact]
+        public void UpdateNote_NullID()
+        {
+            // Arrrange
+            NoteUpdateRequest? noteUpdateRequest =
+                _fixture.Build<NoteUpdateRequest>()
+                .With(tem => tem.Id, (Guid?) null)
+                .Create();
+            //Act 
+            Action action = () => _notesService.UpdateNote(noteUpdateRequest);
+
+            // Assert
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void UpdateNote_IdIsntRight()
+        {
+            // Arrrange
+            NoteUpdateRequest? noteUpdateRequest =
+                _fixture.Build<NoteUpdateRequest>()
+                .With(tem => tem.Id, Guid.NewGuid())
+                .Create();
+            //Act 
+            Action action = () => _notesService.UpdateNote(noteUpdateRequest);
+
+            // Assert
+            action.Should().Throw<ArgumentException>();
+        }
+
+        [Fact]
+        public void UpdateNote_ProperDetails()
+        {
+            // Arrrange
+            NoteAddRequest? noteAddRequest =
+                _fixture.Create<NoteAddRequest>();
+            NoteResponse noteResponse_from_add = _notesService.AddNote(noteAddRequest);
+            NoteUpdateRequest noteUpdateRequest = new NoteUpdateRequest()
+            {
+                Id = noteResponse_from_add.Id,
+                Title = "Updated Title",
+                CreatedTime = DateTime.Now,
+                Description = noteResponse_from_add.Description,
+                DueTime = noteResponse_from_add.DueTime,
+                Status = noteResponse_from_add.Status
+            };
+            Note note= noteUpdateRequest.ToNote();
+            NoteResponse noteResponse = note.ToNoteResponse();
+
+            //Act 
+            NoteResponse noteResponse_from_Update = _notesService.UpdateNote(noteUpdateRequest);
+
+            // Assert
+            noteResponse_from_Update.Should().BeEquivalentTo(noteResponse);
+        }
+        #endregion
+
+        #region DeleteNote
+
+        [Fact]
+        public void DeleteNote_NullId()
+        {
+            // Arrange
+            Guid? id = null;
+
+            //Act
+            Action action = () => _notesService.DeleteNote(id);
+
+            // Assert
+            action.Should().Throw<ArgumentNullException>();
+        }
+
+        [Fact]
+        public void DeleteNote_IdIsntRight_false()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+
+            //Act
+            bool isDeleted = _notesService.DeleteNote(id);
+
+            // Assert
+            isDeleted.Should().BeFalse();
+        }
+
+        [Fact]
+        public void DeleteNote_ProperDetails_true()
+        {
+            // Arrange
+            NoteAddRequest noteAddRequest = _fixture.Create<NoteAddRequest>();
+            NoteResponse noteResponse = _notesService.AddNote(noteAddRequest);
+
+            //Act
+            bool isDeleted = _notesService.DeleteNote(noteResponse.Id);
+
+            // Assert
+            isDeleted.Should().BeTrue();
+        }
+
+
+        #endregion
 
     }
 }
--- To-doTests/obj/Debug/net6.0/To-doTests.AssemblyInfo.cs
+++ To-doTests/obj/Debug/net6.0/To-doTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("To-doTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b6d1435d76c8758a1ebe16e323c1f50b929bc5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf22cf357a06155ef299a2a59761e10520b67b3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("To-doTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("To-doTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
